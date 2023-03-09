0eae90bef38429f42430bbc37783840da9514394
JulianW051 <julianwessels1@gmail.com>
2023-03-09 14:03:58 +0100

    Exercise 3-21

--- Week 2/Week 2/Week 2/Week 2/Program.cs
+++ Week 2/Week 2/Week 2/Week 2/Program.cs
@@ -20,6 +20,10 @@ namespace Week_2
 
             words.ForEach(Console.WriteLine);
             // ---------------------------------------------
+            // Exercise 3-21---------------------------------------------
+            int[] array = { 5, 1, 3, 4, 2 };
+            PrintArrayInStars(array);
+            // ---------------------------------------------
         }
 
         static void RemoveLast(List<string> words)
@@ -27,6 +31,15 @@ namespace Week_2
             if (words.Count > 0)
             {
                 words.RemoveAt(words.Count - 1);
+            }
+        }
+
+        public static void PrintArrayInStars(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                string stars = new string('*', array[i]);
+                Console.WriteLine(stars);
             }
         }
     }
