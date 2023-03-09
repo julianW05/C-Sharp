a2f4514f7def1bc25475dcfd6580da9e8e0edac5
JulianW051 <julianwessels1@gmail.com>
2023-03-09 13:48:59 +0100

    Exercise 3-16

--- Week 2/Week 2/Week 2/Week 2/Program.cs
+++ Week 2/Week 2/Week 2/Week 2/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Week_2
+﻿using Microsoft.VisualBasic;
+
+namespace Week_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            // Exercise 3-16---------------------------------------------
+            List<string> words = new List<string>();
+
+            words.Add("First");
+            words.Add("Second");
+            words.Add("Third");
+
+            words.ForEach(Console.WriteLine);
+
+            RemoveLast(words);
+            RemoveLast(words);
+
+            words.ForEach(Console.WriteLine);
+            // ---------------------------------------------
+        }
+
+        static void RemoveLast(List<string> words)
+        {
+            if (words.Count > 0)
+            {
+                words.RemoveAt(words.Count - 1);
+            }
         }
     }
 }
