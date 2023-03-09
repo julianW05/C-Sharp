4113dc770834ba6f246019ec21785231208986df
JulianW051 <julianwessels1@gmail.com>
2023-03-09 14:14:50 +0100

    exercise 3-28

--- Week 2/Week 2/Week 2/Week 2/Program.cs
+++ Week 2/Week 2/Week 2/Week 2/Program.cs
@@ -24,6 +24,9 @@ namespace Week_2
             int[] array = { 5, 1, 3, 4, 2 };
             PrintArrayInStars(array);
             // ---------------------------------------------
+            // Exercise 3-28---------------------------------------------
+            Opd3_28();
+            // ---------------------------------------------
         }
 
         static void RemoveLast(List<string> words)
@@ -40,6 +43,26 @@ namespace Week_2
             {
                 string stars = new string('*', array[i]);
                 Console.WriteLine(stars);
+            }
+        }
+
+        public static void Opd3_28()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                else
+                {
+                    string[] parts = input.Split(' ');
+                    if (parts.Length > 0)
+                    {
+                        Console.WriteLine(parts[parts.Length - 1]);
+                    }
+                }
             }
         }
     }
